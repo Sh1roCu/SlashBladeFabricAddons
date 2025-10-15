3e5de823ded41df9289eb25476497307c2655a52
mikasa <1244456115@qq.com>
2025-10-15 12:39:57 +0800

    Update

--- src/main/java/cn/sh1rocu/sfaddons/api/extension/IDamageable.java
+++ src/main/java/cn/sh1rocu/sfaddons/api/extension/IDamageable.java
@@ -1,15 +1,10 @@
 package cn.sh1rocu.sfaddons.api.extension;
 
-import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 
 public interface IDamageable {
     default boolean isDamageable(ItemStack stack) {
-        if (!stack.isEmpty() && stack.getItem().getMaxDamage() > 0) {
-            CompoundTag compoundTag = stack.getTag();
-            return compoundTag == null || !compoundTag.getBoolean("Unbreakable");
-        } else {
-            return false;
-        }
+        return ((Item) this).canBeDepleted();
     }
 }
--- src/main/java/cn/sh1rocu/sfaddons/mixin/ItemStackMixin.java
+++ src/main/java/cn/sh1rocu/sfaddons/mixin/ItemStackMixin.java
@@ -1,8 +1,10 @@
 package cn.sh1rocu.sfaddons.mixin;
 
 import cn.sh1rocu.sfaddons.api.extension.IDamageable;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,9 +16,24 @@ public abstract class ItemStackMixin {
     @Shadow
     public abstract Item getItem();
 
+    @Shadow
+    public abstract boolean isEmpty();
+
+    @Shadow
+    @Nullable
+    public abstract CompoundTag getTag();
+
     @Inject(method = "isDamageableItem", at = @At("HEAD"), cancellable = true)
     private void sf$isDamageable(CallbackInfoReturnable<Boolean> cir) {
-        if (this.getItem() instanceof IDamageable damageable)
-            cir.setReturnValue(damageable.isDamageable((ItemStack) (Object) this));
+        if (this.getItem() instanceof IDamageable damageable) {
+            if (this.isEmpty())
+                return;
+            if (damageable.isDamageable((ItemStack) (Object) this)) {
+                CompoundTag compoundtag = this.getTag();
+                cir.setReturnValue(compoundtag == null || !compoundtag.getBoolean("Unbreakable"));
+            } else {
+                cir.setReturnValue(false);
+            }
+        }
     }
 }
